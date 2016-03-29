9a7cb37d56a6e67f8f3cf6883fcf44d59f3b357a
Nir Soffer <nsoffer@redhat.com>
2016-03-29 23:58:05 +0300

    Add log_info macro
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- log.h
+++ log.h
@@ -4,9 +4,7 @@
 extern int debug_mode;
 
 #define log_level(level, fmt, args...) \
-do { \
-    fprintf(stderr, "%s " fmt "\n", level, ##args); \
-} while (0)
+    fprintf(stderr, "%s " fmt "\n", level, ##args)
 
 #define log_debug(fmt, args...) \
 do { \
@@ -14,6 +12,7 @@ do { \
         log_level("DEBUG", fmt, ##args); \
 } while (0)
 
+#define log_info(fmt, args...) log_level("INFO", fmt, ##args)
 #define log_error(fmt, args...) log_level("ERROR", fmt, ##args)
 
 #endif
