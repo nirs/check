ee71d2e23a5eb824ff389173b68218942ba569c1
Nir Soffer <nsoffer@redhat.com>
2016-04-04 02:38:11 +0300

    Add some randomness in the test script
    Not all paths are started at the same time. This should simulate better
    real world usage.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- test/check.py
+++ test/check.py
@@ -4,8 +4,10 @@
 # modify, copy, or redistribute it subject to the terms and conditions
 # of the GNU General Public License v2 or (at your option) any later version.
 
-import subprocess
 import glob
+import random
+import subprocess
+import time
 
 with open("check.log", "a") as log:
     p = subprocess.Popen(["./check", "-d"],
@@ -14,7 +16,8 @@ with open("check.log", "a") as log:
                          stderr=log)
     paths = glob.glob("/rhev/data-center/mnt/*/*/dom_md/metadata")
     for path in paths:
-        p.stdin.write("start %s 1\n" % path)
+        p.stdin.write("start %s 10\n" % path)
+        time.sleep(random.random() * 0.1)
     try:
         p.wait()
     except KeyboardInterrupt:
