94ab2b15fbbd945c64fa9ac187da2cc6e388d8e6
Nir Soffer <nsoffer@redhat.com>
2017-01-28 18:21:43 +0200

    test: Configurable interval and executable
    Allow setting the check interval and the executable from the command
    line, useful for testing c/go versions.

--- test/check.py
+++ test/check.py
@@ -22,14 +22,17 @@ signal.signal(signal.SIGHUP, signal.SIG_IGN)
 signal.signal(signal.SIGINT, terminate)
 signal.signal(signal.SIGTERM, terminate)
 
+interval = int(sys.argv[1])
+cmd = sys.argv[2:]
+
 with open("check.log", "a") as log:
     # Use current stdout to consume process output
-    p = subprocess.Popen(["./check"] + sys.argv[1:],
+    p = subprocess.Popen(cmd,
                          stdin=subprocess.PIPE,
                          stderr=log)
     paths = glob.glob("/rhev/data-center/mnt/*/*/dom_md/metadata")
     for path in paths:
-        p.stdin.write("start %s 1\n" % path)
+        p.stdin.write("start %s %d\n" % (path, interval))
         p.stdin.flush()
     while running:
         try:
